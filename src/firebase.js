62d1228193ccb69ce12bd4a2ee6f12068093575c
Barnabas Molnar <hanta911@gmail.com>
2021-08-27 22:35:05 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYNmCYK-WzilF5IyueQ7pvONBvek5hEoI",
+    authDomain: "netflix2-clone-71ceb.firebaseapp.com",
+    projectId: "netflix2-clone-71ceb",
+    storageBucket: "netflix2-clone-71ceb.appspot.com",
+    messagingSenderId: "229871229440",
+    appId: "1:229871229440:web:f029a13db97026e4b596e5"
+};
